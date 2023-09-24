a51a1d3bffec08ef8323be9c7a733d9dce4a3b86
dazec <dasecojoshua@gmail.com>
2023-09-24 05:28:25 -0800

    added font awesome icon library

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,12 @@ import Add from "./pages/Add";
 import Employee from "./pages/Employee";
 import Update from "./pages/Update";
 import Tree from "./pages/Tree";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="app">
+      <Navbar />
       <BrowserRouter>
         <Routes>
           <Route path="/employee" element={<Employee />} />
